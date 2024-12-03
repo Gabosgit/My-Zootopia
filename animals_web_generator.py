913d04e6b450bb9f76233b7bb4392a6857a475ae
Gabo <no@email>
2024-12-03 16:20:00 +0100

    animal data brought to html file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,24 +5,42 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-
 animals_data = load_data('animals_data.json')
 
+
+def load_html_template(file_path):
+  """ Loads a html file """
+  with open(file_path, "r") as template_html:
+      text_data = template_html.read()
+      return text_data
+
+template_html = load_html_template('animals_template.html')
+#print(template_html)
+
+output = ''  # define an empty string
+
 for animal in animals_data:
-    #print(animal)
+    # append information to each string
     name = animal['name']
-    #print(name)
+    output += f"name: {name}\n"
     diet = animal['characteristics']['diet']
-    #print(diet)
+    output += f"diet: {diet}\n"
     location = animal['locations'][0]
-    #print(location)
-    print(f"name: {name}\n"
-          f"diet: {diet}\n"
-          f"location: {location}"
-          )
+    output += f"location: {location}\n"
     try:
         animal_type = animal['characteristics']['type']
     except KeyError:
-        print()
+        output += f"\n"
     else:
-        print(f"type: {animal_type}\n")
+        output += f"type: {animal_type}\n\n"
+#print(output)
+
+new_html_template = template_html.replace('__REPLACE_ANIMALS_INFO__', output)
+#print(new_html_template)
+
+def write_new_html(file_path):
+  """ Write a html file """
+  with open(file_path, "w") as new_html:
+      new_html.write(new_html_template)
+
+write_new_html('animals.html')
